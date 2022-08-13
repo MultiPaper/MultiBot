9c376adb49ec0205e7d7149d958c4516fc47ddb9
_Pesto <pestonotpasta@gmail.com>
2022-08-13 21:59:58 +0300

    Removed message listener, will debug another time

--- src/events/onMessage.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Message, MessageType } from "discord.js";
-
-export default (client: Client): void => {
-    client.on('messageCreate', (msg: Message) => {
-        if (msg.type == MessageType.UserJoin)
-            msg.react('👋')
-    })
-}
