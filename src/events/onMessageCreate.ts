e4f8e0b2a4c3ab3741675c7da0278b57713042f3
pedrofracassi <pedroluizfracassi@gmail.com>
2023-07-11 10:41:08 -0300

    avoid infinite loops

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -4,6 +4,8 @@ import { errorEmbed } from "../utils/EmbedUtil";
 
 export default (client: Client): void => {
   client.on("messageCreate", async (message: Message) => {
+    if (message.author.bot) return;
+
     if (message.content.toLowerCase().includes('folia')) {
       message.reply('**This is the MultiPaper discord server.**\nFor Folia-related questions, head to the PaperMC server instead: https://discord.gg/papermc')
     }
